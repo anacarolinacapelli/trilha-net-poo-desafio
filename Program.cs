ee4c2c665e98a482d1e4dc3aca96c79d3015212f
Ana Capelli <anacarolinacapellisantos@gmail.com>
2024-05-27 10:48:07 -0300

    Realizando implementações solicitadas conforme diagrama

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+var nokia = new Nokia("123456", "Modelo 1", "111111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone");
+var iphone = new Iphone("987456", "Modelo 2", "222222", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
